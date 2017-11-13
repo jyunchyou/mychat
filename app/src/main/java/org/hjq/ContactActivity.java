f1165b0944cb193675737a65fd55436c6d8e4a2a
jyunchyou <jyunchyou@gmail.com>
2017-11-13 17:49:11 +0800

    添加联系人activity

---
+++ app/src/main/java/org/hjq/ContactActivity.java
@@ -0,0 +1,13 @@
+package org.hjq;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ContactActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+}
+}
+
---
+++ app/src/main/java/org/hjq/ContactWindowActivity.java
@@ -0,0 +1,15 @@
+package org.hjq;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ContactWindowActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
+
+{
+}
